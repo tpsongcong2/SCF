e69dd61a99be134ace7609f1892eec0b3a607d8d
tpsongcong2 <tpsongcong2@gmail.com>
2026-08-21 16:32:51 +0700

    Add files via upload

--- bootstrap.js
+++ bootstrap.js
@@ -15,7 +15,7 @@ document.addEventListener('keydown',function(e){
 
 if('serviceWorker' in navigator&&location.protocol!=='file:'){
   window.addEventListener('load',function(){
-    navigator.serviceWorker.register('./sw.js?v=194')
+    navigator.serviceWorker.register('./sw.js?v=195')
       .then(function(r){})
       .catch(function(e){console.log('SW err:',e);});
   });
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scf-v194';
+const CACHE = 'scf-v195';
 const ASSETS = [
   './',
   './index.html',
