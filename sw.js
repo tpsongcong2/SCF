2f00f5560735cc917f7f679cabfcaaa768be4d53
tpsongcong2 <tpsongcong2@gmail.com>
2026-09-08 16:46:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'scf-v293';
+const CACHE = 'scf-v295';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './runtime.js',
-  './storage.js?v=293',
+  './storage.js?v=295',
   './print-agent.js',
   './helpers.js',
   './defaults.js',
